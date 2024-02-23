79cb7385daa66e031ca66f8ba088f220c7f76f5f
Zaid Kamil <xaidmetamorphos@gmail.com>
2024-02-23 18:46:53 +0530

    Initial commit

---
+++ app/src/main/java/com/digi/jc_500_feb_login/ui/components/myComponents.kt
@@ -0,0 +1,84 @@
+package com.digi.jc_500_feb_login.ui.components
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.digi.jc_500_feb_login.ui.theme.JC_500_Feb_loginTheme
+
+@Composable
+fun UserInputField(
+    modifier: Modifier = Modifier,
+    label: String = "Email",
+    icon: ImageVector = Icons.Default.Email,
+    value: String = "",
+    onValueChange: (String) -> Unit = {},
+    isFocused: Boolean = false,
+) {
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChange,
+        label = { Text(label) },
+        leadingIcon = {
+            Icon(
+                icon,
+                contentDescription = null,
+                modifier = Modifier.padding(start = 24.dp)
+            )
+        },
+        shape = MaterialTheme.shapes.extraLarge,
+        modifier = modifier,
+        colors = TextFieldDefaults.colors(
+            focusedTextColor = Color.Blue,
+            focusedContainerColor = Color.White,
+            unfocusedContainerColor = Color.Transparent,
+        )
+    )
+}
+
+@Composable
+fun RoundedButton(
+    modifier: Modifier = Modifier,
+    text: String = "Log in",
+    onClick: () -> Unit = {},
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier.width(150.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = if (isSystemInDarkTheme()) Color.Yellow else Color.Blue,
+            contentColor = if (isSystemInDarkTheme()) Color.Black else Color.White
+        )
+    ) {
+        Text(text = text)
+    }
+}
+
+
+@Preview
+@Composable
+fun UserInputFieldPreview() {
+    JC_500_Feb_loginTheme {
+        UserInputField(isFocused = true)
+    }
+}
+
+@Preview
+@Composable
+fun ButtonPreview() {
+    RoundedButton(text = "Create")
+}
